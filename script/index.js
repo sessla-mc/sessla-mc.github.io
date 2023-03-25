9658626b65f143de897b849a3aa017ca48f6a5f9
sessla-mc <121537391+sessla-mc@users.noreply.github.com>
2023-03-25 17:31:51 +0900

    Add files via upload

---
+++ script/index.js
@@ -0,0 +1,5 @@
+const body = document.querySelector('.top')
+body.animate([{opacity: '0'}, {opacity: '1'}], 1000)
+
+const h3 = document.querySelector('.big')
+h3.animate([{opacity: '0'}, {opacity: '1'}], 2000) 
