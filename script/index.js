9a1d33598dbec9a4f2ae17c58dcb2b6a5e82ed3e
sessla-mc <121537391+sessla-mc@users.noreply.github.com>
2024-10-20 18:27:15 +0900

    Delete script directory

--- script/index.js
+++
@@ -1,5 +0,0 @@
-const body = document.querySelector('.top')
-body.animate([{opacity: '0'}, {opacity: '1'}], 1000)
-
-const h3 = document.querySelector('.big')
-h3.animate([{opacity: '0'}, {opacity: '1'}], 2000) 
